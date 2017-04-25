c8e818eedd0903c2ba5250b16b33586aec1d2596
David Gutierrez <djgutier@ncsu.edu>
2017-04-24 23:20:58 -0400

    Remove HTML tags from webpages

--- app/models/website_fetcher.rb
+++ app/models/website_fetcher.rb
@@ -1,27 +1,26 @@
 
 class WebsiteFetcher
   require 'net/http'
-  #require 'html/sanitizer'
-
+  
   def initialize(params)
     @url = params["url"]
   end
 
   def FetchContent
+    # http://stackoverflow.com/questions/4581075/how-make-a-http-request-using-ruby-on-rails
     puts "Fetching from website URL: " + @url
     url = URI.parse(@url)
     req = Net::HTTP::Get.new(url.to_s)
     res = Net::HTTP.start(url.host, url.port) {|http|
       http.request(req)
     }
-    res.body
+    sanitize(res.body)
   end
 
-  # private
-  #   def sanitize(html_string)
-  #     sanitizer = Rails::HTML::FullSanitizer.new
-  #     sanitizer.sanitize(html_string)
-  #   end
+  private
+  def sanitize(html_string)
+    ActionController::Base.helpers.strip_tags(html_string)
+  end
 
 end
 
